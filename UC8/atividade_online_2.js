daaedce5ccbf110fc65dff1769ae617e4832720d
Pedro Florenzano <pedroflorenzano.dev@gmail.com>
2021-09-20 19:56:23 -0300

    Add files via upload

---
+++ UC8/atividade_online_2.js
@@ -0,0 +1,24 @@
+const Data = 20;
+const Hoje = 18;
+
+if (Data > Hoje){
+    console.log ('evento permitido');
+} else {
+    console.log('data inválida');
+}
+
+const idade = 22;
+
+if (idade > 18){
+    console.log('permitir cadastro');
+} else {
+    console.log('cadastro não é permitido pela idade');
+}
+
+const participantes = 99;
+
+if(participantes < 100){
+    console.log('permitir cadastro de todos participantes');
+} else {
+    console.log('limite de participantes excedido');
+}
