c11936f30238eaf412d6ca502ae5fd9481e7a812
Pedro Florenzano <pedroflorenzano.dev@gmail.com>
2021-09-20 20:36:18 -0300

    Update atividade_online_2.js

--- UC8/atividade_online_2.js
+++ UC8/atividade_online_2.js
@@ -1,3 +1,10 @@
+/*
+Você desenvolveu o algoritmo para um sistema de cadastro de eventos. Agora, para resolver 
+a situação-problema apresentada, você deve aplicar as técnicas de programação e as ferramentas aprendidas, para implementar esse sistema, 
+por meio da linguagem JavaScript.
+*/
+
+
 const Data = 20;
 const Hoje = 18;
 
