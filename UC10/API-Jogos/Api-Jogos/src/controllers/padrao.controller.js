647971fbc38492f13f05f89c8f7fc5aae966aaaf
PedroFlorenzano <pedroflorenzano.dev@gmail.com>
2021-12-18 15:11:01 -0300

    jogos

--- UC10/API-Jogos/Api-Jogos/src/controllers/padrao.controller.js
+++ UC10/API-Jogos/Api-Jogos/src/controllers/padrao.controller.js
@@ -6,7 +6,7 @@ module.exports = {
   get : (req, res) => {
 
     res.status(200).send({
-      "author" : "Guilherme Felipe Reis Soares",
+      "author" : "Pedro Henrique Damaso Florenzano Gonçalves",
       "description" : "Api feita para um site de jogos"
     });
   }
